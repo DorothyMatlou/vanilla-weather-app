f98ea91f5cfbfc9241c649a7f2f6970426732b51
DorothyMatlou <128805750+DorothyMatlou@users.noreply.github.com>
2023-09-28 09:44:00 +0200

    Updated the Dummy data in JS

--- src/index.js
+++ src/index.js
@@ -14,36 +14,52 @@ const am_pm = now.toLocaleTimeString();
 
 document.getElementById("date").innerHTML = `${day} ${am_pm}`;
 
-function showForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function showForecast(response) {
+  const forecast = response;
   let forecastElement = document.querySelector("#forecast");
-  let forecastHTML = `<div class = "row">`;
-  let days = ["Wed", "Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
-            <div class="col-2">
-              <div class="weather-forecast-day">${day}</div>
-              <div class="weather-forecast-image"><img src="media/weather.png" width="36px" ></div>
-              <div class="weather-forecast-temperatures">
-                <span class="weather-forecast-temperature-max">18°</span>
-                <span class="weather-forecast-temperature-min">11°</span>
-              </div>
-            </div>
-          
-  `;
+  let forecastHTML = `<div class="row">`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML += `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <div class="weather-forecast-image"><img src="https://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png" width="36px"></div>
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max">${Math.round(
+            forecastDay.temp.max
+          )}°</span>
+          <span class="weather-forecast-temperature-min">${Math.round(
+            forecastDay.temp.min
+          )}°</span>
+        </div>
+      </div>
+    `;
+    }
   });
 
-  forecastHTML = forecastHTML + `</div>`;
+  forecastHTML += `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
 
 async function getForecast(coordinates) {
   const apiKey = "597c40c39084687093b091cd48b366f8";
   const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  const response = await fetch(apiUrl + coordinates);
-  var data = await response.json();
-  console.log(data.daily);
+
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  const forecastData = data.daily;
+  showForecast(forecastData);
 }
 
 function convertToFahrenheit(event) {
@@ -102,6 +118,7 @@ async function checkWeather(city) {
 }
 
 checkWeather("Pretoria");
+showForecast();
 
 const form = document.querySelector("#search-form");
 form.addEventListener("submit", (event) => {
